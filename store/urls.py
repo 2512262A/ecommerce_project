b1d06d26520d4038f1d533ecfa2dd1812baaf936
Muhammad Amrullah <2512262a@student.gla.ac.uk>
2021-03-27 08:44:19 +0000

    creating the basic html files, finished.

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('about/', views.about, name="about"),
+    path('faq/', views.faq, name="faq"),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'store/index.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'store/about.html', context)
+
+def faq(request):
+    context = {}
+    return render(request, 'store/faq.html', context)
