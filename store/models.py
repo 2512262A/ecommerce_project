695535ac6ce697fb22894fc7663384b5272899ef
Muhammad Amrullah <2512262a@student.gla.ac.uk>
2021-04-04 19:00:22 +0100

    test commit

--- store/models.py
+++ store/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#  class UserProfile(models.Model):
-    # user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    # name = models.CharField(max_length=150, null=True)
-    # email = models.EmailField(max_length=200)
-
-    # def __str__(self):
-      #  return self.user.username
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
